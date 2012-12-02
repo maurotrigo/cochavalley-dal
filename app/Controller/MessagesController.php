dd84f5f6e2d46ce3804102c45dfad0e694688e29
Miguel Alvaro Molina <alvaromolinac@gmail.com>
2012-12-02 14:09:48 -0400

    sms cambios

--- app/Controller/MessagesController.php
+++ app/Controller/MessagesController.php
@@ -3,6 +3,11 @@ class MessagesController extends AppController {
 	
 	public $components = array('RequestHandler');
 	
+	public function get(){
+        $messages = $this->Message->find('all');
+		$this->set('messages', $messages);
+	}
+	
     public function add($message) {
 		$this->Message->save(array("message"=>$message));
 		$this->autoRender = false;
